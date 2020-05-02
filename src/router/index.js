461427864cd5ecfabc9dc834e99c6788bebb6952
Razon Yang <razonyang@gmail.com>
2020-05-02 18:50:57 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -113,10 +113,9 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes,
-  mode: 'history'
+  routes: constantRoutes
 })
 
 const router = createRouter()
